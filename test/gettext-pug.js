7bc9c9892a3a2ab12943360837bd2f097e22a2c4
Damian Krzeminski <pirxpilot@furkot.com>
2025-07-07 10:39:18 -0600

    use `biome` as a linter and formatter

--- test/gettext-pug.js
+++ test/gettext-pug.js
@@ -1,30 +1,30 @@
-const fs = require('fs');
+const { describe, it } = require('node:test');
+const fs = require('node:fs');
 const gettextPug = require('../');
 
 function read(name) {
   return fs.readFileSync(`${__dirname}/fixtures/${name}`, 'utf8');
 }
 
-describe('gettext-pug', function () {
-  it('should convert jade file', function () {
+describe('gettext-pug', () => {
+  it('should convert jade file', t => {
     const str = read('example.jade');
     const result = gettextPug(str);
     const ref = read('example.js');
-    result.should.eql(ref);
+    t.assert.equal(result, ref);
   });
 
-  it('should convert jade with attributes', function () {
+  it('should convert jade with attributes', t => {
     const str = read('second_attribute.jade');
     const result = gettextPug(str);
     const ref = read('second_attribute.js');
-    result.should.eql(ref);
+    t.assert.equal(result, ref);
   });
 
-  it('should convert ES6 string templates', function () {
+  it('should convert ES6 string templates', t => {
     const str = read('str-template.jade');
     const result = gettextPug(str);
     const ref = read('str-template.js');
-    result.should.eql(ref);
+    t.assert.equal(result, ref);
   });
-
 });
